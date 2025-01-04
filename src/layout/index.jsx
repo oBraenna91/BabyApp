080a45ca4dfb59e92ef525f1fd25e63e0f02f8c5
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2025-01-04 18:05:28 +0100

    Update index.jsx

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { IonPage, IonHeader, IonContent } from '@ionic/react';
+import { IonPage, IonFooter, IonContent } from '@ionic/react';
 import Header from '../components/header';
 
 export default function Layout({ children }) {
   return (
     <IonPage>
-      <IonHeader>
-        <Header />
-      </IonHeader>
       <IonContent>
         {children}
       </IonContent>
+      <IonFooter>
+        <Header/>
+      </IonFooter>
     </IonPage>
   );
 }
